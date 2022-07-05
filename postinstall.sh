55a0d248fd0da6b7b626025c4add07fc7ec48163
David Fridley <ddfridley@yahoo.com>
2022-07-04 20:21:25 -0700

    cypress is optional, bins setup

--- postinstall.sh
+++ postinstall.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-
-echo '*************************************************************************'
-echo POSTINSTALL
-echo '*************************************************************************'
-
-echo '*************************************************************************'
-echo Symbolick link
-echo '*************************************************************************'
-
 mkdir -p ./node_modules/syn
 # on windows environment make sure /tmp exisits so that stream uploads of pictures will work
 mkdir -p /tmp
 mkdir -p ./assets/js
-pwd
-#cp ./node_modules/socket.io-stream/socket.io-stream.js ./assets/js/
 #
 # create the WebComponents index files
 #
@@ -25,16 +14,12 @@ babel app/tools/react-directory-indexer.js --ignore **/__tests__ --out-dir dist/
 node dist/tools/react-directory-indexer.js app/web-components/
 node dist/tools/react-directory-indexer.js --data app/data-components/
 
-echo '*************************************************************************'
-echo TRANSPILE
-echo '*************************************************************************'
-
 npm run transpile  || {
   echo Could not transpile;
   exit 1
 }
 echo "transpile ok"
 
-# packbuild moved to prestart script
+npm run packbuild
 
 
