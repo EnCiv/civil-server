92fab88d877a50537c5d813b9e925efc2a98fecf
Isaac Hudson <ice1080@gmail.com>
2022-10-10 22:56:17 -0600

    cypress test for join, login, and send reset password email

---
+++ cypress/integration/01-user.spec.js
@@ -0,0 +1,35 @@
+describe('User', () => {
+  it('can join the app', () => {
+    cy.visit('/join')
+    cy.contains('Join')
+
+    cy.get('input[name=firstName]').type('Cypress')
+    cy.get('input[name=lastName]').type('User')
+    cy.get('input[name=email]').type('cypress@example.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('svg').click()
+    cy.get('input[name=confirmPassword]').type('password{enter}')
+
+    cy.contains('Welcome Aboard')
+  })
+
+  it('can login after joining', () => {
+    cy.visit('/join')
+    cy.contains('Login').click()
+
+    cy.get('input[name=email]').type('cypress@example.com')
+    cy.get('input[name=password]').type('password{enter}')
+
+    cy.contains('Welcome Aboard')
+  })
+
+  it('can send the reset password email', () => {
+    cy.visit('/join')
+    cy.contains('Login').click()
+
+    cy.get('input[name=email]').type('cypress@example.com')
+    cy.contains('Click here').click()
+
+    cy.contains('Message sent! Please check your inbox')
+  })
+})
