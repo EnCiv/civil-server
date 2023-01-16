3c51cc65bdc2201f2fcf08359623c65a05fb0443
David Fridley <ddfridley@yahoo.com>
2023-01-16 10:54:42 -0800

    do-civil

--- app/tools/civil-package-json.js
+++ app/tools/civil-package-json.js
@@ -20,7 +20,11 @@ function mergeDeps(newPackage, civilPackage, section, changes) {
 
 function main() {
     let newPackage = JSON.parse(fs.readFileSync('./package.json'))
-    const civilPackage = JSON.parse(fs.readFileSync('./node_modules/civil-server/package.json'))
+    const civilServerPackage = JSON.parse(fs.readFileSync('./node_modules/civil-server/package.json'))
+    let civilClientPackage = JSON.parse(fs.readFileSync('./node_modules/civil-client/package.json'))
+    let civilPackage = {}
+    // dependenciers and scrips from the server side will take presidence over those from the client side
+    sections.forEach(section => { civilPackage[section] = { ...civilClientPackage[section], ...civilServerPackage[section] } })
     let changes = { count: 0 }
     sections.forEach(section => mergeDeps(newPackage, civilPackage, section, changes))
     if (newPackage.dependencies['civil-server']) {
---
+++ app/tools/do-civil.sh
@@ -0,0 +1,13 @@
+#/usr/bin/env bash
+echo "Setting up directory template"
+echo "If files exist you will be asked if you want to overwrite them"
+cp -rip ./node_modules/civil-server/template/. .
+echo "extracting node version"
+export NODE_VERSION=$(cat ./node_modules/civil-server/package.json | grep '\"node\":' | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
+nvs add $NODE_VERSION
+source nvs.sh use $NODE_VERSION
+node node_modules/civil-server/app/tools/civil-package-json.js
+if [ ! -f .bashrc ]; then
+    echo "Setting .bshrc"
+    bashrcsetup.sh
+fi
